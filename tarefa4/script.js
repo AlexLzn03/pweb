452c4ee89c9fd0280f7e35d6526213bc0bbec226
AlexLzn03 <alexsanderlozano175474@gmail.com>
2022-03-08 17:37:04 -0300

    tarefa4

---
+++ tarefa4/script.js
@@ -0,0 +1,10 @@
+function OnOffLamp(){
+
+    var img = document.querySelector("#lampada");
+    img.setAttribute('src', 'lampadaligada.png');
+
+    if(src = lampadaligada.png){
+    var img = document.querySelector("#lampada");
+    img.setAttribute('src', 'lampadadesligada.png');
+    }
+}
