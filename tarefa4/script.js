f5b8cde38442ae6e830797741b86b701bb538817
AlexLzn03 <alexsanderlozano175474@gmail.com>
2022-03-18 17:19:49 -0300

    Tarefa4 versão final

--- tarefa4/script.js
+++ tarefa4/script.js
@@ -3,12 +3,12 @@ OnOff = 0
 function OnOffLamp(){
     if(OnOff %2 == 0)
     {
-        document.getElementById("lampada").src="lampadalig.png";
+        document.getElementById("lampada").src="./assets/lampadalig.png";
         document.body.style.background = 'rgb(199, 199, 199)';
     }
     else
     {
-        document.getElementById("lampada").src="lampadadesligada.png";
+        document.getElementById("lampada").src="./assets/lampadadesligada.png";
         document.body.style.background = 'rgb(60, 60, 60)';
     }
     OnOff +=1
