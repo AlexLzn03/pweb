2aa45ce0cd89b86ee90fcc7b3f2afafb5593e614
AlexLzn03 <alexsanderlozano175474@gmail.com>
2022-03-18 17:09:34 -0300

    Tarefa 4 final

--- tarefa4/script.js
+++ tarefa4/script.js
@@ -1,10 +1,15 @@
+OnOff = 0
+
 function OnOffLamp(){
-
-    var img = document.querySelector("#lampada");
-    img.setAttribute('src', 'lampadaligada.png');
-
-    if(src = lampadaligada.png){
-    var img = document.querySelector("#lampada");
-    img.setAttribute('src', 'lampadadesligada.png');
+    if(OnOff %2 == 0)
+    {
+        document.getElementById("lampada").src="lampadalig.png";
+        document.body.style.background = 'rgb(199, 199, 199)';
     }
+    else
+    {
+        document.getElementById("lampada").src="lampadadesligada.png";
+        document.body.style.background = 'rgb(60, 60, 60)';
+    }
+    OnOff +=1
 }
